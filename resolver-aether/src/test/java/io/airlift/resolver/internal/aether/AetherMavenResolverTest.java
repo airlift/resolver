3d5b8a0b92cc94fc3392326ebb36c3cc254579cf
Dain Sundstrom <dain@iq80.com>
2013-02-10 01:07:25 -0800

    Remove dependency between AetherMavenResolverTest and ArtifactResolver

--- resolver-aether/src/test/java/io/airlift/resolver/internal/aether/AetherMavenResolverTest.java
+++ resolver-aether/src/test/java/io/airlift/resolver/internal/aether/AetherMavenResolverTest.java
@@ -10,15 +10,16 @@ import org.testng.annotations.Test;
 import java.io.File;
 import java.util.List;
 
-import static io.airlift.resolver.ArtifactResolver.MAVEN_CENTRAL_URI;
-
 public class AetherMavenResolverTest
 {
+    public static final String USER_LOCAL_REPO = System.getProperty("user.home") + "/.m2/repository";
+    public static final String MAVEN_CENTRAL_URI = "http://repo1.maven.org/maven2/";
+
     @Test
     public void testResolveArtifacts()
             throws Exception
     {
-        AetherMavenResolver aetherMavenResolver = new AetherMavenResolver("target/local-repo", MAVEN_CENTRAL_URI);
+        AetherMavenResolver aetherMavenResolver = new AetherMavenResolver(USER_LOCAL_REPO, MAVEN_CENTRAL_URI);
         List<Artifact> artifacts = aetherMavenResolver.resolveArtifacts(ImmutableList.of(new DefaultArtifact("org.apache.maven:maven-core:3.0.4")));
 
         Assert.assertNotNull(artifacts, "artifacts is null");
@@ -34,7 +35,7 @@ public class AetherMavenResolverTest
         File pomFile = new File("src/test/poms/maven-core-3.0.4.pom");
         Assert.assertTrue(pomFile.canRead());
 
-        AetherMavenResolver aetherMavenResolver = new AetherMavenResolver("target/local-repo", MAVEN_CENTRAL_URI);
+        AetherMavenResolver aetherMavenResolver = new AetherMavenResolver(USER_LOCAL_REPO, MAVEN_CENTRAL_URI);
         List<Artifact> artifacts = aetherMavenResolver.resolvePom(pomFile);
 
         Assert.assertNotNull(artifacts, "artifacts is null");
